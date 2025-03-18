3f8732db054288ebae6e31e5511887ad5559ccf2
Kevin Sivic <kevin.sivic@gmail.com>
2025-03-18 12:48:43 -0400

    API exists to create new contact

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::ContactsController < ApplicationController
     contact = Contact.all
     render json: contact
   end
+
+  def create
+    begin
+      contact = Contact.create!(contact_params)
+      render json: contact
+    rescue ActiveRecord::RecordInvalid => invalid
+      render json: invalid.record.errors
+    end
+  end
+
+  private
+  def contact_params
+    params.permit(:name, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'contacts' => "contacts#index"
+      post 'contacts' => "contacts#create"
     end
   end
   root 'homepage#index'
